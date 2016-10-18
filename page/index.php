39e7f93d375b9ebbdb833a80abe9fd766343cffa
Володимир Чигура < >
2016-10-19 00:15:44 +0300

    lets go!

---
+++ page/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../stylesheets/fontAwesome/css/font-awesome.min.css">
+    <title>Business Tagline goes here</title>
+    <link rel="stylesheet" href="../stylesheets/css/style.css">
+
+</head>
+<body>
+    <header>
+        <div class="wrapp">
+            <div class="fb-logo"><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></div>
+            <div class="hd mail"><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i><span>info@business.co</span></a></div>
+            <div class="hd number"><a href="#"><i class="fa fa-mobile" aria-hidden="true"></i><span>6337 3128</span></a></div>
+        </div>
+    </header>
+</body>
+</html>
